7ba6fe20ded9bf0ecace82c7b7b289b9d94dd925
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2018-12-02 16:45:38 -0500

    ajustements des settings (dummy data)

--- config.js
+++ config.js
@@ -2,9 +2,10 @@ var config = {};
 
 
 config.port = process.env.WEB_PORT || 3000;
-config.dbuser = "jerioux";
-config.dbpassword = "leopassword1";
-config.dbhost = 'mongodb://' + config.dbuser + ':' + config.dbpassword + '@ds123434.mlab.com:23434/leo-rg';
+config.dbuser = "dbusername";
+config.dbpassword = "dbpasswordOfTheUser";
+config.dbaddress =  "bc5555.mlab.com:1234/test-project"
+config.dbhost = 'mongodb://' + config.dbuser + ':' + config.dbpassword + '@' + config.dbaddress ;
 config.title = "BoilerPlate";
 config.slogan = "Entrez un catchphrase sur votre entreprise!";
 
