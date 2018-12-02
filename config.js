fd2024884e9a4934d56dbe5151b3ddf5d326e280
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2018-12-02 16:03:28 -0500

    readme + package.json

--- config.js
+++ config.js
@@ -5,6 +5,8 @@ config.port = process.env.WEB_PORT || 3000;
 config.dbuser = "jerioux";
 config.dbpassword = "leopassword1";
 config.dbhost = 'mongodb://' + config.dbuser + ':' + config.dbpassword + '@ds123434.mlab.com:23434/leo-rg';
-config.title = "Leo-Rg";
+config.title = "BoilerPlate";
+config.slogan = "Entrez un catchphrase sur votre entreprise!";
+
 
 module.exports = config;
--- readme.md
+++ readme.md
@@ -1,2 +1,9 @@
 # Welcome to my Express-Mongo-Pug Boilerplate
 ## Everything's ready with even a signin handler
+
+### Installing the and starting the stack
+`
+$ git clone https://github.com/jelecool/mongo-express-pug-node
+$ npm install
+$ npm start
+`
